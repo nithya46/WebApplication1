1d315079eaa37da2a9097313a1fc2b2e0809d910
nithyapuligundla <nithyapuligundla@gmail.com>
2020-01-19 12:33:02 +0530

    Add project files.

---
+++ WebApplication1/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace WebApplication1
+{
+    public class Class1
+    {
+        
+
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+            Console.WriteLine("This is C# first trail - second time");
+            Console.WriteLine("This is C# first trail - second timeeeeee");
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
